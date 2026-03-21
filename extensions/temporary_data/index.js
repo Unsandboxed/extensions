73e4b8a9a66fbe8406929182e89ac37fe79a03fe
LilyMakesThings <127533508+LilyMakesThings@users.noreply.github.com>
2026-03-21 19:24:51 +0000

    Update Temporary Data

--- extensions/temporary_data/index.js
+++ extensions/temporary_data/index.js
@@ -2,6 +2,7 @@
   "use strict";
 
   const cast = Scratch.UnsandboxedMod.Cast;
+  const stringUtil = new Scratch.UnsandboxedMod.Strings();
 
   /**
    * Unsandboxed blocks for temporary data stored on the executing thread.
@@ -23,7 +24,7 @@
        * The runtime instantiating this block package.
        */
       this.runtime = this.vm.runtime;
-    }
+    };
 
     /**
      * @returns {object} metadata for this extension and its blocks.
@@ -41,6 +42,21 @@
           },
           "---",
           {
+            opcode: "get",
+            color1: "#bc4749",
+            color2: "#bc4749",
+            color4: "#bc4749",
+            blockType: Scratch.BlockType.REPORTER,
+            text: Scratch.translate("[VAR]"),
+            arguments: {
+              VAR: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: Scratch.translate("variable"),
+                menu: "variableGetter",
+              },
+            },
+          },
+          {
             opcode: "set",
             blockType: Scratch.BlockType.COMMAND,
             text: Scratch.translate("set [VAR] to [VALUE]"),
@@ -52,8 +68,8 @@
               },
               VALUE: {
                 type: Scratch.ArgumentType.STRING,
-              }
-            }
+              },
+            },
           },
           {
             opcode: "change",
@@ -67,20 +83,9 @@
               },
               VALUE: {
                 type: Scratch.ArgumentType.NUMBER,
-              }
-            }
-          },
-          {
-            opcode: "get",
-            blockType: Scratch.BlockType.REPORTER,
-            text: Scratch.translate("value of [VAR]"),
-            arguments: {
-              VAR: {
-                type: Scratch.ArgumentType.STRING,
-                defaultValue: Scratch.translate("variable"),
-                menu: "variables",
-              }
-            }
+                defaultValue: 1,
+              },
+            },
           },
           "---",
           {
@@ -101,7 +106,7 @@
               OBJECT: {
                 type: Scratch.ArgumentType.OBJECT,
               },
-            }
+            },
           },
           {
             opcode: "forItem",
@@ -121,7 +126,14 @@
               ARRAY: {
                 type: Scratch.ArgumentType.ARRAY,
               },
-            }
+            },
+          },
+          "---",
+          {
+            opcode: "newScope",
+            blockType: Scratch.BlockType.CONDITIONAL,
+            text: Scratch.translate("new scope"),
+            hideFromPalette: true,
           },
         ],
         menus: {
@@ -129,24 +141,38 @@
             items: "_getAccessibleVariables",
             acceptReporters: true,
             acceptText: true,
+          },
+          variableGetter: {
+            items: "_getAccessibleVariables",
+            acceptReporters: true,
           }
         }
       };
-    }
+    };
 
+    /**
+     * Initialise variables for this thread.
+     * @param {?Thread} thread The thread to initialise variables on.
+     * @returns The variable object.
+     */
     _initVariables(thread) {
       if (!thread.variables) {
         thread.variables = Object.create(null);
       };
 
       return thread.variables;
-    }
+    };
+
+    activeVariables(args, util) {
+      const variables = this._initVariables(util.thread);
+      return Object.keys(variables);
+    };
 
     set(args, util) {
       const variables = this._initVariables(util.thread);
       const name = cast.toString(args.VAR);
       variables[name] = args.VALUE;
-    }
+    };
 
     change(args, util) {
       const variables = this._initVariables(util.thread);
@@ -156,14 +182,60 @@
       const dValue = cast.toNumber(args.VALUE);
       const newValue = castedValue + dValue;
       variables[name] = newValue;
-    }
+    };
 
     get(args, util) {
       const variables = this._initVariables(util.thread);
       const name = cast.toString(args.VAR);
-      return variables[name] ?? "";
-    }
 
+      return variables[name] ?? "";
+    };
+
+    forKeyValue(args, util) {
+      const variables = this._initVariables(util.thread);
+      const keyName = cast.toString(args.KEY);
+      const valueName = cast.toString(args.VALUE);
+
+      if (typeof util.stackFrame.index === "undefined") {
+        util.stackFrame.index = 0;
+      }
+
+      const keys = Object.keys(cast.toObject(args.OBJECT));
+      const values = Object.values(cast.toObject(args.OBJECT));
+
+      if (util.stackFrame.index < keys.length) {
+        variables[keyName] = keys[util.stackFrame.index];
+        variables[valueName] = values[util.stackFrame.index];
+        util.stackFrame.index++;
+        return true;
+      }
+    };
+
+    forItem(args, util) {
+      const variables = this._initVariables(util.thread);
+      const itemName = cast.toString(args.ITEM);
+      const indexName = cast.toString(args.INDEX);
+
+      if (typeof util.stackFrame.index === "undefined") {
+        util.stackFrame.index = 0;
+      }
+
+      const array = cast.toArray(args.ARRAY);
+
+      if (util.stackFrame.index < array.length) {
+        variables[itemName] = array[util.stackFrame.index];
+        variables[indexName] = util.stackFrame.index + 1;
+        util.stackFrame.index++;
+        return true;
+      }
+    };
+
+    /**
+     * Menu constructor for all accessible thread variables.
+     * @param {string} targetId The editing target this menu was initialised from.
+     * @param {?object} menuState An object containing useful info about the menu.
+     * @returns An array containing the known thread variables in this script.
+     */
     _getAccessibleVariables(targetId, menuState) {
       const target = this.runtime.getTargetById(targetId);
       if (!target) return [""];
@@ -174,24 +246,21 @@
       const container = target.blocks;
       const script = container.getTopLevelScript(sourceBlock.id);
 
+      const fields = [
+        `${this.extId}_menu_variables`,
+        `${this.extId}_menu_variableGetter`
+      ]
+
       const values = Object.values(container._blocks)
-        .filter(block => block.opcode === `${this.extId}_menu_variables`)
+        .filter(block => fields.includes(block.opcode))
         .filter(block => container.getTopLevelScript(block.id) === script)
-        .map(block => block.fields.variables.value)
-        .sort(this._compareStrings);
+        .map(block => block.fields[block.opcode
+          .replace(`${this.extId}_menu_`, '')].value)
+        .sort(stringUtil.compareStrings);
 
       if (values.length == 0) return [""];
       return [...new Set(values)];
-    }
-      
-    // an abridged version of Scratch's native comparison script
-    // TODO: is there somewhere in the vm we can point to for this instead?
-    _compareStrings(str1, str2) {
-      return str1.localeCompare(str2, [], {
-        sensitivity: "base",
-        numeric: true,
-      });
-    }
+    };
   }
 
   Scratch.extensions.register(new UnsandboxedTemporaryDataBlocks());
