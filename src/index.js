0e5e63e3f4c540812ae462138e6e4a784906ae74
LilyMakesThings <vgmhut@gmail.com>
2026-04-03 17:30:47 +0100

    create manifests and images

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 const extensionList = require('./extensions.json');
 
 const extensions = Object.create(null);
+const manifests = Object.create(null);
+const images = Object.create(null);
+
+const iconContext = require.context('./extensions', true, /\.(svg|png)$/);
 
 extensionList.forEach(extension => {
-    extensions[extension] = () => require(`./extensions/${extension}/index`);    
-})
+    // Logic for extensions
+    extensions[extension] = () => require(`./extensions/${extension}/index`);
+
+    manifests[extension] = () => require(`./extensions/${extension}/manifest`);
+
+    images[extension] = () => {
+        const svgPath = `./${extension}/icon.svg`;
+        const pngPath = `./${extension}/icon.png`;
+
+        if (iconContext.keys().includes(svgPath)) {
+            return iconContext(svgPath);
+        } else if (iconContext.keys().includes(pngPath)) {
+            return iconContext(pngPath);
+        }
+
+        return require('./unknown.svg');
+    };
+});
 
 module.exports = {
-    extensions
+    extensions,
+    manifests,
+    images
 };
